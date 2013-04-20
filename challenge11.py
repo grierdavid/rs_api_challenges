29f8d78b96661fb5283b15d636ac855c9a6f250f
root <root@muse.(none)>
2013-04-20 21:16:08 +0000

    Challenge complete

--- challenge11.py
+++ challenge11.py
@@ -25,7 +25,6 @@ dns = pyrax.cloud_dns
 cnw = pyrax.cloud_networks
 
 
-
 #Needs to be fqdn and based on a domain associated with this account
 lb_name = 'challb.mymuseisnan.com'
 pvt_nets = []
@@ -35,6 +34,13 @@ imgname = 'CentOS 6.3'
 size = 512
 new_network_name = "Chall_Net"
 new_network_cidr = "192.168.0.0/24"
+
+#key = open('server.key', 'r').readlines()
+#crt = open('server.crt', 'r').readlines() 
+crt = "-----BEGIN CERTIFICATE-----\nMIICaTCCAdICCQDRGeWnEbzt9DANBgkqhkiG9w0BAQUFADB5MQswCQYDVQQGEwJV\nUzELMAkGA1UECAwCVFgxFDASBgNVBAcMC1NhbiBBbnRvbmlvMQ0wCwYDVQQKDARN\naW5lMQ0wCwYDVQQLDARUaGlzMQ0wCwYDVQQDDAR0aGF0MRowGAYJKoZIhvcNAQkB\nFgttZUB0ZXN0LmNvbTAeFw0xMzA0MTcxODA1MjZaFw0xNDA0MTcxODA1MjZaMHkx\nCzAJBgNVBAYTAlVTMQswCQYDVQQIDAJUWDEUMBIGA1UEBwwLU2FuIEFudG9uaW8x\nDTALBgNVBAoMBE1pbmUxDTALBgNVBAsMBFRoaXMxDTALBgNVBAMMBHRoYXQxGjAY\nBgkqhkiG9w0BCQEWC21lQHRlc3QuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCB\niQKBgQDmKGgrjrsURNPdtH36i0lzxffHFniJzp8AfUApQG0P0c1uZ/vUocvh0mCm\nXfpMNfPPNhD6Kwj38wDNBGEBW82bg+//CteFnHzC5ZOoAvxTg5BwARqsh4cIG9TI\n8dEMXe0X5ElhW9nVd3BAaegSibEDLiUt3KYgW7NWQ21jHaT8SQIDAQABMA0GCSqG\nSIb3DQEBBQUAA4GBAMtfi6mQXxy2DUBUQkqO5ahkDz6yWLGmbI/u5AbnUEh793nF\n/bLv+Oj0S7ZwDIbAjJspS7UnnEc7ld1ltzo27p1yjeUQrnAXZEfc9TsiQw3dDrRX\nJBXPkUUbCN95etbnW4Ip0zAwCGsKr37sYAVPlEKrAXJmDOs5tsIHiIHX/ybk\n-----END CERTIFICATE-----\n" 
+
+key = "-----BEGIN RSA PRIVATE KEY-----\nMIICWwIBAAKBgQDmKGgrjrsURNPdtH36i0lzxffHFniJzp8AfUApQG0P0c1uZ/vU\nocvh0mCmXfpMNfPPNhD6Kwj38wDNBGEBW82bg+//CteFnHzC5ZOoAvxTg5BwARqs\nh4cIG9TI8dEMXe0X5ElhW9nVd3BAaegSibEDLiUt3KYgW7NWQ21jHaT8SQIDAQAB\nAoGAWnOu3M9vPnv+N7djez+aUmGPCzMNlDsbyflvacbkFb+CWM2M7vfXRJyFV1y/\nrYvWkazR5z9aeveJbQk3hm0p1CGV2zXhfSs83j9IcvwuzAcWbZNQJQ2j9+Wyr18s\nEUv+/wrq3V2ZiZpyzK0wFrmt6TMgwRdoOwqAJwTEZwzcvcECQQD6q7hEueYvcWJs\nZwcykkCh7NZua5HK115sOFh/kDwqHfuLdUcS5Omr+RzMcFtkuE8G4XH7UjVZGJap\nWRL5TSq9AkEA6w0LiZj17nmwlWpPQ8YIAkF4ouNXqvvc9PZwqphvRbyiCH+75UFX\n5WxVboGTgQEajxej8cY8pTi7BUWdTJR2fQJABrOAgZXWqX4Ge49lidyPDcUfFmSZ\nySpow8vd0csxUI7sopXICKFF/ery8iQuzUqAElcEjQttkawumuhesXt2oQJBAMpD\nSHlYSdCzoLOCbsEISN4ADtpEPa1h3EpjQqJaV+4vm1Mq77ogl41RwtY75H0vQPZ/\nRlGH36nhfnXT6L78RlUCPxx4hCGBDIQOm1ZMzkeLO/gJrLMCfnlD4uwVCqw1osR3\n6CFP7XTNFvTPmoKGabH0VLO5TPl/vMBI7AcRcUCJ9A==\n-----END RSA PRIVATE KEY-----\n" 
+
 
 if len(lb_name.split('.')) < 3:
   print "needs to be a FQDN"
@@ -77,10 +83,14 @@ for i in range(1, 4):
     while not cs.servers.get(server.id).networks:
       time.sleep(1)
     pvt_net = str(cs.servers.get(server.id).networks['private'][0])
-    pub_net = str(cs.servers.get(server.id).networks['public'][0])
+    new_net = str(cs.servers.get(server.id).networks[new_network_name][0])
+    pub_net = [ ip for ip in cs.servers.get(server).networks['public']
+                if len( ip.split(".") ) == 4 ]
+
     pvt_nets.append(pvt_net)
-    print "Public Networks: %s" % pub_net
+    print "Public Networks: %s" % str(pub_net[0])
     print "Private Networks: %s" % pvt_net
+    print "Isolated Networks: %s" % new_net
 
 
 print pvt_nets
@@ -99,6 +109,10 @@ while not "ACTIVE" in clb.get(lb).status:
 lb.add_nodes(nnnode)
 while not "ACTIVE" in clb.get(lb).status:
   time.sleep(1)
+lb.add_ssl_termination(443, key, crt)
+while not "ACTIVE" in clb.get(lb).status:
+  time.sleep(1)
+print "LB SSL Termination added"
 
 #get vip from lb
 for i in lb.virtual_ips:
